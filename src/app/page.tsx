6ef6989901d569a3c2d36bcf29d8417e37dfed87
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-18 13:40:27 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { BentoGridDemo } from "@/components/bento-demo";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <BentoGridDemo />
+    </main>
+  );
+}
